46ac15d10cb1c9ea22afee0101e68885c907069d
Gabriel Carlos Cavalcante <80012329+Gabrielttbr@users.noreply.github.com>
2022-06-11 18:39:40 -0300

    Cadastrado de usario efetuado com sucesso

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Routers
 const routerUser = require('./router/user.router')
 
-app.use(morgan('dev'))
-app.use('/user', routerUser)
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false })) // ACEITA APENAS DADOS SIMPLES
+app.use(bodyParser.json())// JSON DE ENTRADA BODY_PARSER
+
+app.use('/user', routerUser);
 app.use((req,res,next) =>{
     res.status(404).send({
         message: "NOT FOUT"
--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,17 +1,31 @@
-const conectionDatabse = require('../db')
+const conectionDatabse = require('../db');
+const bcrypt = require('bcrypt');
 
-exports.registerUsuario = (req, res, next) => {
-    conectionDatabse.queryMysql('select * from testse').then((result) => {
-        res.status(200).send({
-            message: result
-        })
-    }).catch((e) => {
-        res.status(500).send({
-            erro: e
+exports.registerUsuario = async (req, res, next) => {
+    bcrypt.hash(req.body.senha, 10, (erro, hash) => {
+        if (erro) {
+            return res.status(500).send({
+                error: erro
+            })
+        } else {
+            conectionDatabse.queryMysql('insert into usuario (id_usuario, nome, email, senha, cpf, cep, endereco) values (default, ?,?,?,?,?,?);',
+                [req.body.nome, req.body.email, hash, req.body.cpf, req.body.cep, req.body.endereco])
+                .then((result) => {
+                    res.status(200).send({
+                        message: result
+                    })
+                }).catch((e) => {
+                    res.status(500).send({
+                        erro: e
+                    }
+                    )
+                })
         }
-        )
     })
 }
+
+
+
 
 exports.loginUsuario = (req, res, next) => {
     res.status(200).send({
